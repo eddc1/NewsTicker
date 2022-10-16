5b9009b6ace1643fb9a9852ba73fbb942286378f
Edward <gameeye@protonmail.com>
2022-10-16 12:42:40 +0200

    Navigationsbar Mobile angepasst

--- src/modules/components/NavTest.tsx
+++ src/modules/components/NavTest.tsx
@@ -3,7 +3,6 @@ import { styled, alpha } from '@mui/material/styles';
 import { AppBar, Box, Toolbar, IconButton, InputBase, MenuItem, Menu } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
 import Logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
 import Typography from './Typography';
@@ -17,7 +16,7 @@ const Search = styled('div')(({ theme }) => ({
   '&:hover': {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
-  marginRight: theme.spacing(2),
+  marginRight: theme.spacing(0),
   marginLeft: 0,
   width: '100%',
   height: '80%',
@@ -54,51 +53,23 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 
 
 export default function PrimarySearchAppBar() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
     React.useState<null | HTMLElement>(null);
 
-  const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
 
   const handleMobileMenuClose = () => {
     setMobileMoreAnchorEl(null);
   };
 
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
 
   const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
   const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'left',
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
+  
   //Mobile
 
   const mobileMenuId = 'primary-search-account-menu-mobile';
@@ -107,7 +78,7 @@ export default function PrimarySearchAppBar() {
       anchorEl={mobileMoreAnchorEl}
       anchorOrigin={{
         vertical: 'top',
-        horizontal: 'right',
+        horizontal: 'left',
       }}
       id={mobileMenuId}
       keepMounted
@@ -118,32 +89,15 @@ export default function PrimarySearchAppBar() {
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
-      {/*
-      <MenuItem>
-        <IconButton size="large" aria-label="show 100 new mails" color="inherit">
-          <Badge badgeContent={100} color="error">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton
-          size="large"
-          aria-label="show 100 new notifications"
-          color="inherit"
-        >
-          <Badge badgeContent={100} color="error">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-      */}
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <p>Profile</p>
-      </MenuItem>
-
+      {pages.map((page) => (
+        <MenuItem
+              key={page}>          
+            <Link style={{ textDecoration: "none", color: "black", paddingRight: 30 }} to={`/${page}`}>
+            {page}
+            </Link>
+        
+        </MenuItem>
+      ))}
     </Menu>
   );
 // Mobile Ende
@@ -176,35 +130,6 @@ export default function PrimarySearchAppBar() {
                 inputProps={{ 'aria-label': 'search' }}
               />
             </Search>
-            {/*
-            <IconButton size="large" aria-label="show 100 new mails" color="inherit">
-              <Badge badgeContent={100} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            */}
-            {/*
-            <IconButton
-              size="large"
-              aria-label="show 100 new notifications"
-              color="inherit"
-            >
-              <Badge badgeContent={100} color="error">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-             */}
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
           </Box>
           <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
             <IconButton
@@ -221,7 +146,6 @@ export default function PrimarySearchAppBar() {
         </Toolbar>
       </AppBar>
       {renderMobileMenu}
-      {renderMenu}
     </Box>
   );
 }
